85ef65db0a22cc25fa555c26de2998e9a92d3c54
davidGBbr <davidgomesbrigido@gmail.com>
2024-07-19 13:30:09 -0300

    create a user

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import { v4 as uuid } from 'uuid';
+import { hashSync as bcryptHashSync } from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  private readonly users: UserDto[] = [];
+
+  createUser(user: UserDto) {
+    const newUser = {
+      id: uuid(),
+      username: user.username,
+      password: bcryptHashSync(user.password, 10),
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+}
